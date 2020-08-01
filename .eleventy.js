ce6ab1eba2c5ecd23cd1f9b5074fc3b973cc9ec8
Ajit Panigrahi <ajitzero@gmail.com>
2020-08-02 01:21:15 +0530

    :heavy_plus_sign: Added eleventy svg plugin

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,13 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const svgContents = require("eleventy-plugin-svg-contents");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(svgContents);
 
   eleventyConfig.setDataDeepMerge(true);
 
